09483d14735b94778b7f370985a1ea938b7ab7e4
Dimichanga <92356906+Dimichanga@users.noreply.github.com>
2022-03-03 11:46:34 +0300

    Add files via upload

--- calculation.py
+++ calculation.py
@@ -2,26 +2,29 @@ from classes import time_, geometry, Boundary
 import openpyxl
 
 def conductivity (str1, str2, str3, onedimens = geometry, Left = Boundary, Right = Boundary, times = time_):
-    
+
+    with open (str1,'r') as f:
+        initcond = float(f.read())
+    with open (str2, "r") as f:
+        coef = float(f.read())
     flag = True 
     while flag:
         numb = int(input("Enter the number of steps in the space(by default enter 10): "))
         tau = float(input("Enter time step for the calculation (by default enter 0.5): "))
         h = (onedimens.length / numb) 
-        if tau <= 0.5*(h**2): 
+        if tau <= (h**2) / (2*coef): 
             flag = False 
         else:
             print("Error! Stability condition is not met")
-    
+
     Nl=len(Left.table)
     Nr=len(Right.table)
     k = 0
     u=[None] * (numb)
     u_=[None] * (numb)
-    with open (str1,'r') as f:
-        initcond = float(f.read())
-    with open (str2, "r") as f:
-        coef = float(f.read())
+    alpha = [None] * (numb)
+    beta = [None] * (numb)
+
     with open (str3, "w") as f:
         f.write("Left boundary: \n")
         for i in range(len(Left.table)):
@@ -48,7 +51,9 @@ def conductivity (str1, str2, str3, onedimens = geometry, Left = Boundary, Right
     while times.output < times.step:
         times.output = float(input("Error! the step for output cannot be less than the step for calculation. Enter time step for the output (by default enter 0.5): "))
     times.set_n()
-    gamma = times.step * coef / (h**2)
+    A = -coef / (h**2)
+    B = (1/tau) + (2*coef/(h**2))
+    C = A
         
     u[0] = Left.table[0][1]
     u[numb-1] = Right.table[0][1]
@@ -81,11 +86,19 @@ def conductivity (str1, str2, str3, onedimens = geometry, Left = Boundary, Right
         for i in range (numb):
             u_[i] = u[i]
         
+        alpha[1] = 0
+        beta[1] = Left.get(times.actual)
         times.actual += times.step
         u[0] = Left.get(times.actual)
         u[numb-1] = Right.get(times.actual)
+
         for i in range (1,numb-1):
-            u[i] = gamma * u_[i-1] + (1 - 2 * gamma) * u_[i] + gamma * u_[i + 1] 
+            d = u_[i] / tau
+            alpha[i+1] = (-A) / (B + C*alpha[i])
+            beta[i+1] = (d - C*beta[i]) / (B + C*alpha[i])
+        for i in range (numb-2,0,-1):
+            u[i] = alpha[i+1]*u[i+1] + beta[i+1]
+
         k += 1
 
     sheet.cell(row = count + 3, column = 1).value = times.actual
